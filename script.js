ecaafc6d9d1f7604da25800ee80cd6fd65a42951
reyner paniagua jimenez <you@example.com>
2021-03-01 15:29:44 -0600

    Commit E

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let sideNav = document.querySelector('.side-nav');
 let toggler = document.querySelector('.toggler');
 let closeBtn = document.querySelector('.close-btn');
-
+//Menu Scripts
 toggler.addEventListener('click', ()=>{
     sideNav.style.left = '0';
 });
