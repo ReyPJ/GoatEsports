e9c4e1865d0eb0f230f6bc77aaffe830ca8a60c6
reyner paniagua jimenez <you@example.com>
2021-03-08 02:11:02 -0600

    Commit

--- displays.js
+++ displays.js
@@ -6,32 +6,48 @@ let fifaDisplay = document.querySelector('.teams-content-main-fifa')
 //Codigo//
 
 function SetlolDisplay(){
-    if (lolDisplay.style.display === 'none'){
+    if (lolDisplay.style.display === 'none' || tftDisplay.style.display === 'flex' || 
+        apexDisplay.style.display === 'flex' || fifaDisplay.style.display === 'flex'){
         lolDisplay.style.display = 'flex';
+        tftDisplay.style.display = 'none';
+        fifaDisplay.style.display = 'none';
+        apexDisplay.style.display = 'none';
     } else {
         lolDisplay.style.display = 'none';
     }
 };
 
 function SetTftDisplay(){
-    if (tftDisplay.style.display === 'none'){
+    if (tftDisplay.style.display === 'none' || lolDisplay.style.display === 'flex' || 
+        apexDisplay.style.display === 'flex' || fifaDisplay.style.display === 'flex'){
         tftDisplay.style.display = 'flex';
+        lolDisplay.style.display = 'none';
+        fifaDisplay.style.display = 'none';
+        apexDisplay.style.display = 'none';
     } else {
         tftDisplay.style.display = 'none';
     }
 };
 
 function SetApexDisplay(){
-    if (apexDisplay.style.display === 'none'){
+    if (apexDisplay.style.display === 'none' || lolDisplay.style.display === 'flex' || 
+    tftDisplay.style.display === 'flex' || fifaDisplay.style.display === 'flex'){
         apexDisplay.style.display = 'flex';
+        lolDisplay.style.display = 'none';
+        fifaDisplay.style.display = 'none';
+        tftDisplay.style.display = 'none';
     } else {
         apexDisplay.style.display = 'none';
     }
 };
 
 function SetFifaDisplay(){
-    if (fifaDisplay.style.display === 'none'){
+    if (fifaDisplay.style.display === 'none' || lolDisplay.style.display === 'flex' || 
+    tftDisplay.style.display === 'flex' || apexDisplay.style.display === 'flex'){
         fifaDisplay.style.display = 'flex';
+        lolDisplay.style.display = 'none';
+        apexDisplay.style.display = 'none';
+        tftDisplay.style.display = 'none';
     } else {
         fifaDisplay.style.display = 'none';
     }
